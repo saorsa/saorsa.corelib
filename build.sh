551025ff538cba19638d334e5cc9629ae1b6828a
Atanas Dragolov <adragolov@gmail.com>
2022-06-03 14:15:36 +0300

    Adding build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+echo "🤖 NetCore environment: "
+dotnet --info
+dotnet --list-sdks
+
+echo "🤖 Restoring packages: "
+dotnet restore ./src/
+
+for path in ./src/**/*.csproj; do
+    echo "🤖 Building project at path ${path}: "
+    dotnet build -c Release ${path}
+done
+
+echo "🤖 Running tests..."
+for path in tests/**/*.csproj; do
+    echo " * Testing project at path ${path}: "
+    dotnet test -f net6.0 -c Release ${path}
+done
